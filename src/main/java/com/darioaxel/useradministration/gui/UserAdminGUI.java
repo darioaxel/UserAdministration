aa71cd3f28b2fa90039d751f68ffaa23a1b2c577
darioaxel <darioaxel@gmail.com>
2020-05-05 16:30:04 +0200

    Added JavaFX GUI

---
+++ src/main/java/com/darioaxel/useradministration/gui/UserAdminGUI.java
@@ -0,0 +1,17 @@
+package com.darioaxel.useradministration.gui;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author darioaxel
+ */
+public class UserAdminGUI {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
