037908f6c7d8d3931f4ff348a0496709cde63ac5
darioaxel <darioaxel@gmail.com>
2020-05-07 13:28:41 +0200

    Working fxml with controller! Finally! need to test methods calls

--- src/main/java/com/darioaxel/useradministration/gui/App.java
+++ src/main/java/com/darioaxel/useradministration/gui/App.java
@@ -5,7 +5,11 @@
  */
 package com.darioaxel.useradministration.gui;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -18,13 +22,21 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    	  Parent root = null;
+  		// We need to add a try catch to avoid Exceptions if the fxml document doesn't exist.
+      	try {
+      		// FXMLLoader join our Front View FXML document (Main.fxml) and the Control Java class (MainController.java)
+      		
+  			root = FXMLLoader.load(getClass().getResource("/com/darioaxel/useradministration/gui/views/Main.fxml"));
+  			
+  		} catch (IOException e) {
+  			System.out.print("EXCEPCION: No es capaz de encontrar Main.fxml");
+  			// TODO Auto-generated catch block
+  			e.printStackTrace();
+  		}
+           Scene scene = new Scene(root);
+           stage.setScene(scene);
+           stage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/darioaxel/useradministration/gui/controllers/MainController.java
+++ src/main/java/com/darioaxel/useradministration/gui/controllers/MainController.java
@@ -2,7 +2,7 @@ package com.darioaxel.useradministration.gui.controllers;
 
 import javafx.fxml.FXML;
 
-public class MainControl {
+public class MainController {
 	@FXML
 	public void accionButtonStart() {
 		
