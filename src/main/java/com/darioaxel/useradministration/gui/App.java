1474b1841260ddca7757b3b638b9ed1c96eeb263
darioaxel <darioaxel@gmail.com>
2021-01-28 18:28:05 +0100

    Added another method showing mouse events

--- src/main/java/com/darioaxel/useradministration/gui/App.java
+++ src/main/java/com/darioaxel/useradministration/gui/App.java
@@ -6,6 +6,8 @@
 package com.darioaxel.useradministration.gui;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -20,24 +22,26 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
-    @Override
-    public void start(Stage stage) {
-    	  Parent root = null;
-  		// We need to add a try catch to avoid Exceptions if the fxml document doesn't exist.
-      	try {
-      		// FXMLLoader join our Front View FXML document (Main.fxml) and the Control Java class (MainController.java)
-      		
-  			root = FXMLLoader.load(getClass().getResource("/com/darioaxel/useradministration/gui/views/Main.fxml"));
-  			
-  		} catch (IOException e) {
-  			System.out.print("EXCEPCION: No es capaz de encontrar Main.fxml");
-  			// TODO Auto-generated catch block
-  			e.printStackTrace();
-  		}
-           Scene scene = new Scene(root);
-           stage.setScene(scene);
-           stage.show();
-    }
+	 @Override
+
+		 public void start(Stage stage) {
+	    	  Parent root = null;
+	  		// We need to add a try catch to avoid Exceptions if the fxml document doesn't exist.
+	      	try {
+	      		// FXMLLoader join our Front View FXML document (Main.fxml) and the Control Java class (MainController.java)
+	      		
+	  			root = FXMLLoader.load(getClass().getResource("/com/darioaxel/useradministration/gui/views/Main.fxml"));
+	  			
+	  		} catch (IOException e) {
+	  			System.out.print("EXCEPCION: No es capaz de encontrar Main.fxml");
+	  			// TODO Auto-generated catch block
+	  			e.printStackTrace();
+	  		}
+	           Scene scene = new Scene(root);
+	           stage.setScene(scene);
+	           stage.show();
+	    }
+	    
 
     public static void main(String[] args) {
         launch();
--- src/main/java/com/darioaxel/useradministration/gui/controllers/MainController.java
+++ src/main/java/com/darioaxel/useradministration/gui/controllers/MainController.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class MainController {
 	
@@ -18,7 +21,23 @@ public class MainController {
 	TextField startTextField;
 	
 	@FXML
+	Rectangle basicRectangle;
+	
+	@FXML
 	public void saySomething(ActionEvent event) {
 		startTextField.setText("Hey! ho! Let's go!!!");
+		System.out.println(event.toString());
+	}
+	
+	@FXML
+	public void changeColorOnEnter() {
+		System.out.println("On enter");
+		basicRectangle.setFill(Color.RED);		
+	}
+	
+	@FXML
+	public void changeColorOnExit() {
+		System.out.println("On exit");
+		basicRectangle.setFill(Color.BLUE);		
 	}
 }
